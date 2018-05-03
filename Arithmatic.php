a4ab74e61530a5811d64bdbc659922fa62af6c6c
hafizafaq3 <hafizafaq3@gmail.com>
2018-05-03 17:01:14 +0500

    commit

---
+++ Arithmatic.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Arithmatic Rules</title>
+</head>
+<body>
+
+<?php
+
+echo "<h2>Learning PHP in Punjab University</h2>";
+
+$x = 10;
+$y = 20;
+echo 'Sum of Two `Numbers is: ';
+echo $x + $y;
+echo "<br>";
+
+$x = 10;
+$y = 20;
+echo 'Difference between Two `Numbers is: ';
+echo $y - $x;
+echo "<br>";
+
+$x = 10;
+$y = 20;
+echo 'Product of Two `Numbers is: ';
+echo $x * $y;
+echo "<br>";
+
+$x = 10;
+$y = 20;
+echo 'Division of Two `Numbers is: ';
+echo $y / $x;
+echo "<br>";
+
+$x = 10;
+$y = 20;
+echo 'Performing Modulus: ';
+echo $y % $x;
+echo "<br>";
+
+echo "<h2>Printing Table of 8 using For Loop</h2>";
+for ($x = 1; $x <= 10; $x++)
+{
+	echo "8 x $x = ";
+	echo $x * 8;
+	echo "<br>";
+}
+
+echo "<h2>Even</h2>";
+
+?>
+
+</body>
+</html>
